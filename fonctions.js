f3bc6d312f5a67fb1c9773f8b16ca8aaf487b1ef
Michael de Ruijter <yougi8000@gmail.com>
2018-09-20 17:35:26 +0200

    atelier2

---
+++ fonctions.js
@@ -0,0 +1,59 @@
+Exercice 1
+
+function name() {
+   console.log("Hello World");
+}
+name();
+
+Exercice 2
+
+function nameoffunction(x) {
+   console.log(x);
+}
+nameoffunction("name");
+
+Exercice 3
+
+function shaker(number) {
+   let result=0;
+   result=number * 2;
+   return (result);
+}
+
+Exercice 4
+
+function shaker(arg1, arg2) {
+   let result=0;
+   result = arg1 + arg2;
+   console.log(result);
+}
+shaker
+
+
+Exercice 5
+
+function nameoffunction(x) {
+   console.log("*"+ x +"*");
+}
+nameoffunction("John")
+   
+
+
+
+
+
+Créer un fichier fonctions.js
+
+Ecrire des fonctions pour :
+
+    Simplement afficher la chaîne “Hello World!”
+
+    Renvoyer à l’identique le paramètre passé en entrée
+
+    Renvoyer le double de la valeur entière passée en entrée
+
+    Renvoyer l’addition de deux valeurs entières passées en entrée
+
+    Renvoyer, à partir d’une chaîne de caractères passée en entrée, la même chaîne entourée du caractère *  (‘John’ => ‘*John*’)
+
+
